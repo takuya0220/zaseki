cbd4fdd14e3268d3a7720a68df4568328beadf99
Takuya Maeda <maeda_t@japacom.co.jp>
2018-08-31 20:45:31 +0900

    現在の環境をとりあえずコミット

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,69 @@
-//{app}/gulpfile.js
-
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var browserSync = require('browser-sync');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-csso');
+var sourcemaps = require('gulp-sourcemaps');
+// var pug = require('gulp-pug');
+// var less = require('gulp-less');
+// var concat = require('gulp-concat');
+// var coffee = require('gulp-coffee');
+// var uglify = require('gulp-uglify');
+// var imagemin = require('gulp-imagemin');
+var del = require('del');
 
-function reload() {
-    browserSync.reload({
-        stream: false
-    });
+var paths = {
+    javascripts: [
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/leaflet/dist/leaflet.js'
+    ],
+    stylesheets: [
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/leaflet/dist/leaflet.css'
+    ],
+    images: ''
 };
 
-gulp.task('browsersync', function() {
-    browserSync.init({
-        files: ['public/**/*.*', 'views/**/*.*'], // BrowserSyncにまかせるファイル群
-        proxy: 'http://localhost:3000', // express の動作するポートにプロキシ
-        port: 4000, // BrowserSync は 4000 番ポートで起動
-        open: true
-    });
+// Not all tasks need to use streams
+// A gulpfile is just another node program and you can use any package available on npm
+gulp.task('clean', function() {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    return del(['public/javascripts', 'public/stylesheets']);
 });
 
-gulp.task('serve', ['browsersync'], function() {
-    nodemon({
-        script: './bin/www',
-        ext: 'js html css',
-        ignore: [ // nodemon で監視しないディレクトリ
-            'node_modules',
-            'bin',
-            'views',
-            'public',
-            'test'
-        ],
-        env: {
-            'NODE_ENV': 'development'
-        },
-        stdout: false // Express の再起動時のログを監視するため
-    }).on('readable', function() {
-        this.stdout.on('data', function(chunk) {
-            if (/^Express\ server\ listening/.test(chunk)) {
-                // Express の再起動が完了したら、reload() でBrowserSync に通知。
-                // ※Express で出力する起動時のメッセージに合わせて比較文字列は修正
-                reload();
-            }
-            process.stdout.write(chunk);
-        });
-        this.stderr.on('data', function(chunk) {
-            process.stderr.write(chunk);
-        });
-    });
+gulp.task('javascripts', ['clean'], function() {
+    return gulp.src(paths.javascripts)
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/javascripts'));
 });
 
-gulp.task('default', ['serve']);
+gulp.task('stylesheets', ['clean'], function() {
+    return gulp.src(paths.stylesheets)
+        .pipe(minifyCSS())
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/stylesheets'));
+});
+
+// Copy all static images
+gulp.task('images', ['clean'], function() {
+    // return gulp.src(paths.images)
+    // Pass in options to the task
+    // .pipe(imagemin({
+    //     optimizationLevel: 5
+    // }))
+    // .pipe(gulp.dest('build/img'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    // gulp.watch(paths.scripts, ['javascripts']);
+    // gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'javascripts', 'stylesheets', 'images']);
